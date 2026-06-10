d5d66118ea156cc2357196a797e16c49089c1981
R. Tharshihan <tharshihan2000@gmail.com>
2026-06-10 14:44:54 +0530

    Update index.jsfrontend url updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-     "https://ticket-booking-we7zrpzp5-tharshihan-s-projects.vercel.app",
+     "https://ticket-booking-blue-seven.vercel.app",
     ],
     credentials: true,
   })
